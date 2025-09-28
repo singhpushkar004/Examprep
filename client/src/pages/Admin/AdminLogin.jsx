546be543da7ec0c3e23ef9d749fa946d3157d581
Pushkar singh <126847807+singhpushkar004@users.noreply.github.com>
2025-09-28 15:07:16 +0530

    Update AdminLogin.jsx

--- client/src/pages/Admin/AdminLogin.jsx
+++ client/src/pages/Admin/AdminLogin.jsx
@@ -15,7 +15,7 @@ const AdminLogin = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const res = await axios.post('http://localhost:5000/api/admin/login', form);
+        const res = await axios.post('https://examprep-dja4.onrender.com/api/admin/login', form);
 
         if (res.data.message === "Login Successfully") {
             localStorage.setItem("role", res.data.admin.role);
